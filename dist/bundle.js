3befd9e70ccb81f4348cf3587916d3d5486eb887
bosomFriends <2261600550@qq.com>
2021-04-04 00:25:47 +0800

    rollup基本用法

---
+++ dist/bundle.js
@@ -0,0 +1,12 @@
+var LFL = (function (exports) {
+	'use strict';
+
+	const time = new Date();
+
+	exports.time = time;
+
+	Object.defineProperty(exports, '__esModule', { value: true });
+
+	return exports;
+
+}({}));
